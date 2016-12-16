6e1c4785e7c47418c70a2d516c61c89d7d059c62
xu1718191411 <xu1718191411@gmail.com>
2016-12-16 18:58:03 +0900

    a

--- bin/sessionSocketsRelease.js
+++ bin/sessionSocketsRelease.js
@@ -320,6 +320,145 @@ var sessionSockets = function(sessionSockets,steps,mongo,io) {
             })()
         })
 
+        socket.on("rate",function(msg){
+            var num = session.debateLogin.num
+            var rNum = session.debateLogin.rNum
+            var position = session.debateLogin.position
+            var rate = msg.rate
+            var _update
+            var debateResult
+            var pro,con,group
+            var proUpdate = {}
+            var conUpdate = {}
+            steps(function(){
+                mongo.find("debateStatus",{num:num,rNum:rNum},{},this.hold(function(result){
+                    if(result.length == 0) return;
+
+                    pro = result[0].pro
+                    con = result[0].con
+                    group = result[0].group
+
+                    if(result[0].status == "finish" || result[0].status == "noAnalysisfinish"){
+                        if(position == 1){
+
+                            debateResult = determine(rate,result[0].conRate)
+
+                            if(!result[0].proRate){
+                                //如果你是正方,且还没有对自己做评价,那么接下来对自己做评价
+                                if(debateResult != "wait" && debateResult != "unknow"){
+                                    _update = {$set:{proRate:rate,debateResult:debateResult,finish:true}}
+                                }else{
+                                    _update = {$set:{proRate:rate,debateResult:debateResult}}
+                                }
+
+                            }
+                        }else if(position == 2){
+
+                            debateResult = determine(result[0].proRate,rate)
+                            if(!result[0].conRate){
+                                //如果你是反方,且还没有对自己做评价,那么接下来对自己做评价
+
+                                if(debateResult != "wait" && debateResult != "unknow"){
+                                    _update = {$set:{conRate:rate,debateResult:debateResult,finish:true}}
+                                }else{
+                                    _update = {$set:{conRate:rate,debateResult:debateResult}}
+                                }
+
+                            }
+                        }else{
+                            this.terminate()
+                        }
+                    }
+                }))
+            },function(){
+                mongo.update("debateStatus",{num:num,rNum:rNum},_update,{},this.hold(function(_result){
+
+                }))
+            },function(){
+
+                if(debateResult != "wait" && debateResult != "unknow"){
+                    if(debateResult == "proWin"){
+                        proUpdate = {$inc:{winNum:1}}
+                        conUpdate = {$inc:{lostNum:1}}
+                    }
+
+                    if(debateResult == "conWin"){
+                        proUpdate = {$inc:{lostNum:1}}
+                        conUpdate = {$inc:{winNum:1}}
+                    }
+
+                    if(debateResult == "draw"){
+                        proUpdate = {$inc:{drawNum:1}}
+                        conUpdate = {$inc:{drawNum:1}}
+                    }
+
+                    if(debateResult == "unable"){
+                        proUpdate = {$inc:{unDeterminNum:1}}
+                        conUpdate = {$inc:{unDeterminNum:1}}
+                    }
+
+                }
+
+            },function(){
+                //update pro
+                /*
+                 mongo.update("debateMembers",{username:pro,group:group},proUpdate,{},this.hold(function(){
+
+                 }))
+
+                 */
+
+            },function(){
+                //update con
+                /*
+                 mongo.update("debateMembers",{username:con,group:group},conUpdate,{},this.hold(function(){
+
+                 }))
+                 */
+            },function(){
+                mongo.find("debateStatus",{num:num,rNum:rNum},{},this.hold(function(result){
+                    socket.emit("rated",{err:0,data:{proRate:result[0].proRate,conRate:result[0].conRate}})
+                    socket.to(session.debateLogin.rNum).broadcast.emit("rated",{err:0,data:{proRate:result[0].proRate,conRate:result[0].conRate}})
+                }))
+            })()
+
+
+
+            function determine(proRes,conRes){
+                if(!proRes || !conRes){
+                    return "wait"
+                }
+
+                if(proRes == 1 && conRes == 3){
+                    return "proWin"
+                }
+
+                if(proRes == 3 && conRes == 1){
+                    return "conWin"
+                }
+
+                if(proRes == 2 && conRes == 2){
+                    return "draw"
+                }
+
+                if(proRes == 1 && (conRes == 1 || conRes == 2)){
+                    return "unable"
+                }
+
+                if(proRes == 3 && (conRes == 2 || conRes == 3)){
+                    return "unable"
+                }
+
+                if(proRes == 2 && (conRes == 1 || conRes == 3)){
+                    return "unable"
+                }
+
+                return "unknow"
+
+            }
+
+        })
+
         socket.on("confirmAnalysisResult",function(msg){
             var pro
             var con
